1eef97b5530fe06be2060fd60f1fb7d967f3259b
Leenamkwon <namkwon12@gmail.com>
2020-04-09 00:58:39 +0900

    update color

--- paintingCanvas/app.js
+++ paintingCanvas/app.js
@@ -1,44 +1,59 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
-canvas.width = "500";
-canvas.height = "500";
+const colors = document.getElementsByClassName('jsColor');
+
+canvas.width = 500
+canvas.height = 500
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
-
-function stopPainting() {
+// console.log(painting);
+function stopPainting(e) {
   painting = false;
 }
 
-// 마우스 이동
-function onMouseMove(e) {
-  const x = e.offsetX;
-  const y = e.offsetY;
-
-  if(!painting) {
-    ctx.beginPath(); // 경로 생성
-    ctx.moveTo(x, y); // 펜을 x와 y로 지정된 좌표로 옮김
-  } else {
-    ctx.lineTo(x, y); // 시작점은 이전에 그려진 경로에 의해 결정되며, 시작점은 moveTo()메소드를 통해 변경가능
-    ctx.stroke(); // 윤곽선 
-  }
-}
-
-// 마우스 누르기
 function startPainting() {
   painting = true;
 }
 
-// 마우스 떼기
-function onmouseDown(e) {
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  // console.log(painting);
+
+  if(!painting) { // 클릭하고 움직이면 이 것은 작동하지 않음.
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    console.log(painting);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    console.log(painting);
+  }
+}
+
+function onmousedown(e) {
   painting = true;
 }
 
-if(canvas) {
+function onMouseUp(e) {
+  stopPainting();
+}
+
+function changeColor(e) {
+  const color = e.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+const jsColor = [...colors];
+jsColor.forEach(color => color.addEventListener('click', changeColor));
