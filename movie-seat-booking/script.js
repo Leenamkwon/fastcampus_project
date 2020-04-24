ac00a6521389ea2e6b59fe782700199a3c693e93
Leenamkwon <namkwon12@gmail.com>
2020-04-24 21:37:28 +0900

    local storage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -3,19 +3,48 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const movieSelect = document.getElementById('moive');
+const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movideIndex, MovidePrice) {
+  localStorage.setItem('selectedMovieIndex', movideIndex);
+  localStorage.setItem('selectedMoviePrice', MovidePrice);
+}
 
 // Update total and count
-function updateSelectedCount() {}
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  console.log(selectedSeats);
 
+  // Copy selected seats into arr
+  // Map through array
+  // return a new array index
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-    console.log(e.target);
     e.target.classList.toggle('selected');
 
-    //Count 기능
     updateSelectedCount();
   }
 });
