38aaa334815b1be1027f0cfc47b5d89ca0f47a44
Leenamkwon <namkwon12@gmail.com>
2020-05-10 10:39:10 +0900

    밑바닥 부터 다시 만들어보기

--- simple project clone/countdown/setup/app.js
+++ simple project clone/countdown/setup/app.js
@@ -12,21 +12,22 @@ const months = [
   'November',
   'December'
 ];
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.deadline-format h4');
+const items = deadline.querySelector('.deadline-format h4');
 
-let tempDate = new Date();
-let tempYear = tempDate.getFullYear();
-let tempMonth = tempDate.getMonth();
-let tempDay = tempDate.getDate();
+const futureDate = new Date(2020, 4, 27, 30, 0);
 
-console.log(tempDay);
-// let futureDate = new Date(2020, 2, 03, 10, 50, 0);
-console.log(futureDate);
-const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -37,53 +38,4 @@ const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
-
-// future time in ms
-// 데드라인 시간 가져오기
-const futureTime = futureDate.getTime();
-
-function getRemainingTime() {
-  const today = new Date().getTime();
-  const t = futureTime - today;
-  // 1s = 1000ms
-  // 1m = 60s
-  // 1hr = 60min
-  // 1d = 24hr
-
-  // values in ms
-  const oneDay = 24 * 60 * 60 * 1000;
-  // console.log(oneDay);
-  const oneHour = 60 * 60 * 1000;
-  const oneMinute = 60 * 1000;
-
-  // calculate all values
-  let days = t / oneDay;
-  days = Math.floor(days);
-  let hours = Math.floor((t % oneDay) / oneHour);
-  let minutes = Math.floor((t % oneHour) / oneMinute);
-  let seconds = Math.floor((t % oneMinute) / 1000);
-
-  // set values arrays
-  const values = [days, hours, minutes, seconds];
-
-  function format(item) {
-    if (item < 10) {
-      return (item = `0${item}`);
-    }
-    return item;
-  }
-
-  items.forEach((item, index) => {
-    item.innerHTML = format(values[index]);
-  });
-
-  if (t < 0) {
-    clearInterval(countdown);
-  }
-}
-
-// countdown
-let countdown = setInterval(getRemainingTime, 1000);
-
-getRemainingTime();
+giveaway.textContent = `giveaway ends on ${weekday} ${month} ${year} ${hours} : ${minutes}am`;
--- simple project clone/tabs/setup/app.js
+++ simple project clone/tabs/setup/app.js
@@ -3,18 +3,18 @@ const about = document.querySelector('.about');
 const articles = document.querySelectorAll('.content');
 
 about.addEventListener('click', (e) => {
-  e.preventDefault();
   const id = e.target.dataset.id;
+
   if (id) {
-    btns.forEach((btn) => {
-      btn.classList.remove('active');
+    btns.forEach((item) => {
+      item.classList.remove('active');
       e.target.classList.add('active');
     });
-    // hide other articles
-    articles.forEach((text) => {
-      text.classList.remove('active');
-      const element = document.getElementById(id);
-      element.classList.add('active');
+    articles.forEach((item) => {
+      item.classList.remove('active');
+      if (id === item.id) {
+        item.classList.add('active');
+      }
     });
   }
 });
