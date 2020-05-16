cb6a0e8603d003bcfc045cb02ed3fbb1bc5c45a6
Leenamkwon <namkwon12@gmail.com>
2020-05-16 13:48:20 +0900

    addMeal fun

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -9,7 +9,7 @@ const search = document.getElementById('search'),
 function searchMeal(e) {
   e.preventDefault();
 
-  // Clear single meal
+  // clear single meal
   single_mealEl.innerHTML = '';
 
   // Get search term
@@ -17,66 +17,53 @@ function searchMeal(e) {
 
   // Check for empty
   if (term.trim()) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        resultHeading.innerHTML = `<h2>Search results for ${term}:</h2>`;
+        resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
 
         if (data.meals === null) {
-          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+          resultHeading.innerHTML = `<p>There are no search results. Try again</p>`;
         } else {
           mealsEl.innerHTML = data.meals
             .map((meal) => {
-              return `
-              <div class="meal">
-                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              return `<div class="meal">
+                <img src="${meal.strMealThumb}" alt="${meal.strMealThumb}" />
                 <div class="meal-info" data-mealID="${meal.idMeal}">
                   <h3>${meal.strMeal}</h3>
                 </div>
-              </div>
-            `;
+            </div>`;
             })
             .join('');
         }
       });
-    // clear search text
+    // Clear search value
     search.value = '';
   } else {
-    alert('Please enter a search term');
+    alert('Please enter a search value');
   }
 }
 
 // Fetch meal by ID
-function getMealById(mealID) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+function getMealByID(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
     .then((res) => res.json())
     .then((data) => {
       const meal = data.meals[0];
+
+      console.log(meal);
 
       addMealToDOM(meal);
     });
 }
 
-// get random Meal
-function getRandomMeal() {
-  // clear meals and heading
-  mealsEl.innerHTML = '';
-  resultHeading.innerHTML = '';
-
-  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then((res) => res.json())
-    .then((data) => {
-      const meal = data.meals[0];
-
-      addMealToDOM(meal);
-    });
-}
-
-// Add meal to DOM
+// Add meal the DOM
 function addMealToDOM(meal) {
   const ingredients = [];
 
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 1; i <= 20; i += 1) {
     if (meal[`strIngredient${i}`]) {
       ingredients.push(
         `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
@@ -87,27 +74,15 @@ function addMealToDOM(meal) {
   }
 
   single_mealEl.innerHTML = `
-    <div class="single-meal">
-      <h1>${meal.strMeal}</h1>
-      <img src="${meal.strMealThumb}" alt="${meal.strMealThumb}" />
-      <div class="single-meal-info">
-        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
-        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
-      </div>
-      <div class="main">
-        <p>${meal.strInstructions}</p>
-        <h2>Ingredients</h2>
-        <ul>
-          ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
-        </ul>
-      </div>
-    </div>
+    <ul>
+      ${ingredients.map((list) => list).join('')}
+    </ul>
   `;
 }
 
-// Event Listeners
+// Event listeners
 submit.addEventListener('submit', searchMeal);
-mealsEl.addEventListener('click', (e) => {
+meals.addEventListener('click', (e) => {
   const mealInfo = e.path.find((item) => {
     if (item.classList) {
       return item.classList.contains('meal-info');
@@ -117,9 +92,7 @@ mealsEl.addEventListener('click', (e) => {
   });
 
   if (mealInfo) {
-    const mealID = mealInfo.getAttribute('data-mealID');
-    getMealById(mealID);
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealByID(mealID);
   }
 });
-
-random.addEventListener('click', getRandomMeal);
