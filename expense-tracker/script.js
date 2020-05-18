d5992cd73e9d772417ed1e53eee193099806ecd9
Leenamkwon <namkwon12@gmail.com>
2020-05-18 23:04:23 +0900

    밑바닥 부터 다시 만듦

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -6,14 +6,19 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-];
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(localStorage.getItem('listItem'));
+
+let transactions =
+  localStorage.getItem('listItem') !== null ? localStorageTransactions : [];
+
+console.log(transactions);
 
 // Add transactions to DOM List
 function addTransactionDOM(transaction) {
@@ -26,7 +31,9 @@ function addTransactionDOM(transaction) {
   item.innerHTML = `
     ${transaction.text} <span>${sign}$${Math.abs(
     transaction.amount
-  )}</span> <button class="delete-btn">x</button>
+  )}</span> <button class="delete-btn" onclick="removeList(${
+    transaction.id
+  })">x</button>
   `;
   list.appendChild(item);
 }
@@ -48,12 +55,59 @@ function updateValues() {
   ).toFixed(2);
 
   balance.innerText = total;
-  income.innerText = income;
-  expense.innerText = expense;
+  money_plus.innerText = income;
+  money_minus.innerText = expense;
+}
+
+function inputValue(e) {
+  e.preventDefault();
+  const textValue = text.value;
+  const amountValue = amount.value;
+
+  if (textValue.trim() === '' || amountValue.trim() === '') {
+    alert('Please adding value');
+  } else {
+    const dataObj = {
+      id: getRandomID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(dataObj);
+
+    addTransactionDOM(dataObj);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+function getRandomID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+function removeList(id) {
+  transactions = transactions.filter((item) => item.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('listItem', JSON.stringify(transactions));
 }
 
 function init() {
+  list.innerHTML = '';
   transactions.forEach((item) => addTransactionDOM(item));
+  updateValues();
 }
 
 init();
+
+form.addEventListener('submit', inputValue);
