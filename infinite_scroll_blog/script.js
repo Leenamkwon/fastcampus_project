745f36132d5ca396d0d04bdd49d13a6dc9180a53
Leenamkwon <namkwon12@gmail.com>
2020-05-20 14:15:14 +0900

    add loading css, infinite post

--- infinite_scroll_blog/script.js
+++ infinite_scroll_blog/script.js
@@ -12,6 +12,7 @@ async function getPosts() {
   );
 
   const data = await res.json();
+  console.log(data);
 
   return data;
 }
@@ -20,17 +21,18 @@ async function getPosts() {
 async function showPosts() {
   const posts = await getPosts();
 
-  posts.forEach(post => {
+  posts.forEach((post) => {
     const postEl = document.createElement('div');
     postEl.classList.add('post');
     postEl.innerHTML = `
-      <div class="number">${post.id}</div>
-      <div class="post-info">
+    <div class="number">${post.id}</div>
+    <div class="post-info">
+      <div class="post-body">
         <h2 class="post-title">${post.title}</h2>
         <p class="post-body">${post.body}</p>
       </div>
+    </div>
     `;
-
     postsContainer.appendChild(postEl);
   });
 }
@@ -46,24 +48,7 @@ function showLoading() {
       page++;
       showPosts();
     }, 300);
-  }, 1000);
-}
-
-// Filter posts by input
-function filterPosts(e) {
-  const term = e.target.value.toUpperCase();
-  const posts = document.querySelectorAll('.post');
-
-  posts.forEach(post => {
-    const title = post.querySelector('.post-title').innerText.toUpperCase();
-    const body = post.querySelector('.post-body').innerText.toUpperCase();
-
-    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
-      post.style.display = 'flex';
-    } else {
-      post.style.display = 'none';
-    }
-  });
+  }, 1400);
 }
 
 // Show initial posts
@@ -72,9 +57,10 @@ showPosts();
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
+  // clientHeight은 view화면 높이
+  // scrollHeight은 scroll 화면 전체 높이
+  // scrollTop은 scroll 높이에서 화면의 top값
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
   }
 });
-
-filter.addEventListener('input', filterPosts);
