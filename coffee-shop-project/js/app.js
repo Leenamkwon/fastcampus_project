59ced3def1df39fee83d1a53e7931423e2cb19ff
Leenamkwon <namkwon12@gmail.com>
2020-05-25 22:51:28 +0900

    drink input validator fn

--- coffee-shop-project/js/app.js
+++ coffee-shop-project/js/app.js
@@ -8,7 +8,7 @@ function UI() {}
 UI.prototype.hidePreloader = function () {
   setInterval(() => {
     document.querySelector('.preloader').style.display = 'none';
-  }, 1000);
+  }, 500);
 };
 
 // show nav
@@ -30,6 +30,43 @@ UI.prototype.videoControl = function () {
   }
 };
 
+UI.prototype.checkEmpty = function (name, lastname, email) {
+  let result;
+
+  if (name === '' || lastname === '' || email === '') {
+    result = false;
+  } else {
+    result = true;
+  }
+  return result;
+};
+
+UI.prototype.showFeedback = function (text, type) {
+  let feedback = document.querySelector('.drink-form__feedback');
+
+  if (type === 'success') {
+    feedback.classList.add('success');
+    feedback.innerText = text;
+    this.removeAlert('success');
+  } else if (type === 'error') {
+    feedback.classList.add('error');
+    feedback.innerText = text;
+    this.removeAlert('error');
+  }
+};
+
+UI.prototype.removeAlert = function (type) {
+  setTimeout(() => {
+    document.querySelector('.drink-form__feedback').classList.remove(type);
+  }, 3000);
+};
+
+function Customer(name, lastname, email) {
+  this.name = name;
+  this.lastname = lastname;
+  this.email = email;
+}
+
 const ui = new UI();
 
 function eventListeners() {
@@ -43,6 +80,24 @@ function eventListeners() {
 
   document.querySelector('.video__switch').addEventListener('click', () => {
     ui.videoControl();
+  });
+
+  // submit the form
+  document.querySelector('.drink-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector('.input-name').value;
+    const lastName = document.querySelector('.input-lastname').value;
+    const email = document.querySelector('.input-email').value;
+
+    let value = ui.checkEmpty(name, lastName, email);
+
+    if (value) {
+      let display = new Customer();
+      ui.showFeedback('good job', 'success');
+    } else {
+      ui.showFeedback('some form values empty', 'error');
+    }
   });
 }
 
