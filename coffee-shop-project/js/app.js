81a15be19ba819b7614014c9383541430d47f86b
Leenamkwon <namkwon12@gmail.com>
2020-05-25 18:51:36 +0900

    video play, pause

--- coffee-shop-project/js/app.js
+++ coffee-shop-project/js/app.js
@@ -4,10 +4,30 @@
 
 function UI() {}
 
+// show preloader
 UI.prototype.hidePreloader = function () {
   setInterval(() => {
     document.querySelector('.preloader').style.display = 'none';
   }, 1000);
+};
+
+// show nav
+UI.prototype.showNav = function () {
+  document.querySelector('.nav').classList.toggle('nav--show');
+};
+
+// play , pause video
+UI.prototype.videoControl = function () {
+  let btn = document.querySelector('.video__switch-btn');
+  const video = document.querySelector('.video__item');
+
+  if (!btn.classList.contains('btnSlide')) {
+    btn.classList.add('btnSlide');
+    video.pause();
+  } else {
+    btn.classList.remove('btnSlide');
+    video.play();
+  }
 };
 
 const ui = new UI();
@@ -16,8 +36,13 @@ function eventListeners() {
   window.addEventListener('load', () => {
     ui.hidePreloader();
   });
+
   document.querySelector('.navBtn').addEventListener('click', () => {
-    document.querySelector('.nav').classList.toggle('nav--show');
+    ui.showNav();
+  });
+
+  document.querySelector('.video__switch').addEventListener('click', () => {
+    ui.videoControl();
   });
 }
 
