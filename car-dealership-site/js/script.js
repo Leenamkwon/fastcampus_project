ce17006b637fc08b38aa558e071b3b1e314c4f89
Leenamkwon <namkwon12@gmail.com>
2020-05-26 23:40:45 +0900

    바닐라로 데이터 만드는 법

--- car-dealership-site/js/script.js
+++ car-dealership-site/js/script.js
@@ -2,7 +2,87 @@ window.addEventListener('load', () =>
   document.querySelector('.preloader').classList.add('hidePreloader')
 );
 
+// 데이터 만드는 법
 const CreateCars = (() => {
   // car data
   const cars = [];
+
+  // car class
+  class Car {
+    constructor(make, country, img, special, model, price, type, trans, gas) {
+      this.make = make;
+      this.country = country;
+      this.img = img;
+      this.special = special;
+      this.model = model;
+      this.price = price;
+      this.type = type;
+      this.trans = trans;
+      this.gas = gas;
+    }
+  }
+
+  // car creatation function
+  function makeCar(
+    make,
+    country,
+    img = './img/car-default.jpeg',
+    special = true,
+    model = 'new model',
+    price = 10000,
+    type = 'sedan',
+    trans = 'automatic',
+    gas = '50'
+  ) {
+    const car = new Car(
+      make,
+      country,
+      img,
+      special,
+      model,
+      price,
+      type,
+      trans,
+      gas
+    );
+    cars.push(car);
+  }
+
+  // produce cars
+  function produceCar() {
+    makeCar('chevy', 'american');
+    makeCar('mercedes', 'german', './img/car-german-1.jpeg', true);
+    makeCar('mercedes', 'german', './img/car-german-2.jpeg');
+    makeCar(
+      'mercedes',
+      'german',
+      './img/car-german-3.jpeg',
+      false,
+      'some model'
+    );
+    makeCar(
+      'mercedes',
+      'german',
+      './img/car-german-4.jpeg',
+      undefined,
+      'some model'
+    );
+    makeCar('mercedes', 'german', './img/car-german-5.jpeg', false);
+    makeCar('chevy', 'american', './img/car-american-1.jpeg');
+    makeCar('chevy', 'american', './img/car-american-2.jpeg', false);
+    makeCar('chevy', 'american', './img/car-american-3.jpeg', false);
+    makeCar('chevy', 'american', './img/car-american-4.jpeg', false);
+    makeCar('chevy', 'american', './img/car-american-5.jpeg', false);
+  }
+  produceCar();
+  //   console.log(cars);
+  const specialCars = cars.filter((car) => car.special === true);
+  //   console.log(specialCars);
+
+  return {
+    cars,
+    specialCars
+  };
 })();
+
+console.log(CreateCars);
