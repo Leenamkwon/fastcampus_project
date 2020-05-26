d5f79ff5cc5942423538ac60ac2133c0cbf8fc7a
Leenamkwon <namkwon12@gmail.com>
2020-05-27 00:29:21 +0900

    arcodian imgbox

--- car-dealership-site/js/script.js
+++ car-dealership-site/js/script.js
@@ -52,16 +52,10 @@ const CreateCars = (() => {
   function produceCar() {
     makeCar('chevy', 'american');
     makeCar('mercedes', 'german', './img/car-german-1.jpeg', true);
-    makeCar('mercedes', 'german', './img/car-german-2.jpeg');
+    makeCar('bmw', 'german', './img/car-german-2.jpeg');
+    makeCar('bmw', 'german', './img/car-german-3.jpeg', false, 'some model');
     makeCar(
-      'mercedes',
-      'german',
-      './img/car-german-3.jpeg',
-      false,
-      'some model'
-    );
-    makeCar(
-      'mercedes',
+      'bmw',
       'german',
       './img/car-german-4.jpeg',
       undefined,
@@ -85,4 +79,36 @@ const CreateCars = (() => {
   };
 })();
 
-console.log(CreateCars);
+const DisplaySpecialCars = ((CreateCars) => {
+  const specialCars = CreateCars.specialCars;
+
+  const info = document.querySelector('.featured-info');
+
+  document.addEventListener('DOMContentLoaded', () => {
+    info.innerHTML = '';
+
+    let data = '';
+
+    specialCars.forEach((item) => {
+      data += `<div
+        class="featured-item my-3 d-flex p-2 text-capitalize align-items-baseline flex-wrap" data-img="./img/${item.img}"
+      >
+        <span data-img="./${item.img}" class="featured-icon mr-2">
+          <i class="fas fa-car"></i>
+        </span>
+        <h5 class="font-weight-bold mx-1">${item.make}</h5>
+        <h5 class="mx-1">${item.model}</h5>
+      </div>`;
+    });
+    info.innerHTML = data;
+  });
+
+  // change img
+  info.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.parentElement.classList.contains('featured-icon')) {
+      const img = event.target.parentElement.dataset.img;
+      document.querySelector('.featured-photo').src = img;
+    }
+  });
+})(CreateCars);
