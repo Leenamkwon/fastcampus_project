3e62f11be888cf1fb861b2e77bac3a60d1d9e0ed
Leenamkwon <namkwon12@gmail.com>
2020-05-27 11:32:24 +0900

    강사 코드가 ux적으로 마음에 안들어서 사용자입장에서 임의로 만들어봄

--- car-dealership-site/js/script.js
+++ car-dealership-site/js/script.js
@@ -71,7 +71,7 @@ const CreateCars = (() => {
   produceCar();
   //   console.log(cars);
   const specialCars = cars.filter((car) => car.special === true);
-  //   console.log(specialCars);
+  console.log(specialCars);
 
   return {
     cars,
@@ -83,6 +83,7 @@ const DisplaySpecialCars = ((CreateCars) => {
   const specialCars = CreateCars.specialCars;
 
   const info = document.querySelector('.featured-info');
+  console.log(info);
 
   document.addEventListener('DOMContentLoaded', () => {
     info.innerHTML = '';
@@ -101,14 +102,101 @@ const DisplaySpecialCars = ((CreateCars) => {
       </div>`;
     });
     info.innerHTML = data;
+
+    // 밑에 코드 마음에 안들어서 ux위주로 편하게 따로 바꿔봄
+    const category = document.querySelectorAll('.featured-item');
+    category.forEach((img) => {
+      img.addEventListener('click', (e) => {
+        const childSpan = e.currentTarget.firstElementChild.dataset.img;
+        document.querySelector('.featured-photo').src = childSpan;
+      });
+    });
   });
 
-  // change img
-  info.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (e.target.parentElement.classList.contains('featured-icon')) {
-      const img = event.target.parentElement.dataset.img;
-      document.querySelector('.featured-photo').src = img;
-    }
+  // // change img
+  // info.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   if (e.target.parentElement.classList.contains('featured-icon')) {
+  //     const img = e.target.parentElement.dataset.img;
+  //     document.querySelector('.featured-photo').src = img;
+  //   }
+  // });
+})(CreateCars);
+
+const DisplayCars = ((CreateCars) => {
+  // all cars
+  const cars = CreateCars.cars;
+  // car container
+  const inventory = document.querySelector('.inventory-container');
+
+  // content loaded
+  document.addEventListener('DOMContentLoaded', () => {
+    inventory.innerHTML = '';
+
+    let output = '';
+    cars.forEach((car) => {
+      output += `<!-- single car -->
+      <div class="col-10 mx-auto my-3 col-md-6 col-lg-4 single-car ${car.country}">
+        <div class="card car-card">
+          <img
+            src="${car.img}"
+            class="card-img-top car-img"
+            alt=""
+          />
+          <!-- card body -->
+          <div class="card-body">
+            <div class="car-info d-flex justify-content-between">
+              <!-- first flex child -->
+              <div class="car-text text-uppercase">
+                <h6 class="font-weight-bold">${car.make}</h6>
+                <h6>${car.model}</h6>
+              </div>
+              <!-- second flex child -->
+              <h5 class="car-value align-self-center py-2 px-3">
+                $
+                <span class="car-price">${car.price}</span>
+              </h5>
+            </div>
+          </div>
+          <!-- end of card -->
+          <div
+            class="card-footer text-capitalize d-flex justify-content-between"
+          >
+            <p>
+              <span><i class="fas fa-car"></i></span>${car.type}
+            </p>
+            <p>
+              <span><i class="fas fa-cogs"></i></span>${car.trans}
+            </p>
+            <p>
+              <span><i class="fas fa-gas-pump"></i></span>${car.gas}
+            </p>
+          </div>
+        </div>
+      </div>`;
+    });
+
+    inventory.innerHTML = output;
   });
 })(CreateCars);
+
+const FilterCars = (() => {
+  const filter = document.querySelectorAll('.filter-btn');
+
+  filter.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const value = e.target.dataset.filter;
+      const singleCar = document.querySelectorAll('.single-car');
+
+      singleCar.forEach((car) => {
+        if (value === 'all') {
+          car.style.display = 'block';
+        } else {
+          !car.classList.contains(value)
+            ? (car.style.display = 'none')
+            : (car.style.display = 'block');
+        }
+      });
+    });
+  });
+})();
