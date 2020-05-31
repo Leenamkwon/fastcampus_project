26fa1b4032ec282cd4ffd3029bf032af3f7e97f2
Leenamkwon <namkwon12@gmail.com>
2020-05-31 15:31:30 +0900

    repeat several application project

--- simple project clone/typing-game/script.js
+++ simple project clone/typing-game/script.js
@@ -32,81 +32,57 @@ const words = [
   'loving'
 ];
 
-// init word
+// Init word
 let randomWord;
 
 // Init score
 let score = 0;
 
-// Init Time
+// Init time
 let time = 10;
-
-// hard Mode
-let difficulty =
-  localStorage.getItem('difficulty') !== null
-    ? localStorage.getItem('difficulty')
-    : 'medium';
-
-// Set difficulty select value
-difficultySelect.value =
-  localStorage.getItem('difficulty') !== null
-    ? localStorage.getItem('difficulty')
-    : 'medium';
-
-// Settings select
-settingsForm.addEventListener('change', (e) => {
-  difficulty = e.target.value;
-  localStorage.setItem('difficulty', difficulty);
-});
 
 // Focus on text on start
 text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
 
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
-// Add to the DOM
+// Add word to DOM
 function addWordToDOM() {
   randomWord = getRandomWord();
-  word.innerText = randomWord;
+  word.innerHTML = randomWord;
 }
+addWordToDOM();
 
-// update score
 function updateScore() {
   score += 1;
   scoreEl.innerHTML = score;
 }
 
-// start counting down
-function timeless() {
-  timeEl.innerText = time + 's';
-
+function updateTime() {
+  time -= 1;
+  timeEl.innerHTML = `${time}s`;
   if (time === 0) {
-    clearInterval(secondsTime);
+    clearInterval(timeInterval);
+    // end game
     gameOver();
   }
 }
 
-const secondsTime = setInterval(() => {
-  time -= 1;
-  timeless();
-}, 1000);
-
-// game over, show end screen
+// Game over, show end screen
 function gameOver() {
   endgameEl.innerHTML = `
-  <h1>Time ran out</h1>
-  <p>Your final score is ${score}</p>
-  <button onclick="location.reload()">Reload</button>
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
   `;
-  // Location.reload() 메서드는 새로고침 버튼처럼 현재 리소스를 다시 불러옵니다.
-
   endgameEl.style.display = 'flex';
 }
-
-addWordToDOM();
 
 // Event listeners
 text.addEventListener('input', (e) => {
@@ -114,23 +90,12 @@ text.addEventListener('input', (e) => {
 
   if (insertedText === randomWord) {
     addWordToDOM();
-    updateScore();
-    timeless();
 
     // Clear
     e.target.value = '';
 
-    if (difficulty === 'hard') {
-      time += 2;
-    } else if (difficulty === 'medium') {
-      time += 3;
-    } else {
-      time += 5;
-    }
+    time += 5;
 
-    timeless();
+    updateTime();
   }
 });
-
-// Settings btn click
-settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
