992a49642ff716559cae66f56db169f23059f3d0
Leenamkwon <namkwon12@gmail.com>
2020-06-04 17:47:02 +0900

    객체지향을 확실하게 연마하기 위해 기존에 반복해서 쓰는 펑션을 더욱 세분화해서 쪼갰으며 반복적인 문을 리팩토링 하고 로컬에 저장하여 post, put 기능을 추가해보았다.  뿌듯..

--- simple project clone/flashCard/js/app.js
+++ simple project clone/flashCard/js/app.js
@@ -1,4 +1,3 @@
-// event listeners
 function eventListeners() {
   const showBtn = document.getElementById('show-btn');
   const questionCard = document.querySelector('.question-card');
@@ -11,136 +10,119 @@ function eventListeners() {
   let data = [];
   let id = 1;
 
-  // new ui instance
   const ui = new UI();
 
-  // show question form
-  showBtn.addEventListener('click', function () {
+  showBtn.addEventListener('click', () => {
     ui.showQuestion(questionCard);
   });
-
-  // hide question form
-  closeBtn.addEventListener('click', function () {
+  closeBtn.addEventListener('click', () => {
     ui.hideQuestion(questionCard);
   });
-
-  // add question
-  form.addEventListener('submit', function (e) {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const questionValue = questionInput.value;
     const answerValue = answerInput.value;
 
-    if (questionValue === '' || answerValue === '') {
-      feedback.classList.add('showItem', 'alert-danger');
-      feedback.textContent = 'cannot add empty values';
-
-      setTimeout(function () {
-        feedback.classList.remove('showItem', 'alert-danger');
-      }, 3000);
+    if (questionValue.trim() === '' || answerValue.trim() === '') {
+      ui.feedback('값을 입력하세요', feedback);
     } else {
-      const question = new Question(id, questionValue, answerValue);
-      data.push(question);
-      id++;
-      ui.addQuestion(questionList, question);
+      const questionData = {
+        id: id++,
+        question: questionValue,
+        answerInput: answerValue
+      };
+      data.push(questionData);
+      ui.addQuestion(questionData, questionList);
       ui.clearFields(questionInput, answerInput);
+      localStorage.setItem('list', JSON.stringify(data));
     }
   });
-  // work with a question
-  questionList.addEventListener('click', function (e) {
+  questionList.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.classList.contains('delete-flashcard')) {
-      let id = e.target.dataset.id;
-      questionList.removeChild(
-        event.target.parentElement.parentElement.parentElement
-      );
-      data = data.filter((item) => {
-        return item.id !== +id;
-      });
-    } else if (e.target.classList.contains('show-answer')) {
-      e.target.nextElementSibling.classList.toggle('showItem');
+      ui.deleteQuestion(questionList, e.target, data);
     } else if (e.target.classList.contains('edit-flashcard')) {
-      // delete question
-      let id = e.target.dataset.id;
-
-      questionList.removeChild(
-        event.target.parentElement.parentElement.parentElement
-      );
-
-      // show the question card
-      ui.showQuestion(questionCard);
-      // specific question
-      const tempQuestion = data.filter((item) => item.id === +id);
-      // rest of the data
-      data = data.filter((item) => {
-        return item.id !== +id;
-      });
-      questionInput.value = tempQuestion[0].title;
-      answerInput.value = tempQuestion[0].answers;
     }
+  });
+  const local = JSON.parse(localStorage.getItem('list'));
+  local.forEach((item) => {
+    ui.addQuestion(item, questionList);
   });
 }
 
 // ui constructor
 function UI() {}
 
+function Question() {}
+
 // show question card
 UI.prototype.showQuestion = function (element) {
   element.classList.add('showItem');
 };
-
 // hide question card
 UI.prototype.hideQuestion = function (element) {
   element.classList.remove('showItem');
 };
-
-// add question
-UI.prototype.addQuestion = function (element, question) {
-  const div = document.createElement('div');
-  const { id, title, answer } = question;
-  div.classList.add('col-md-4');
-  div.innerHTML = `
-  <div class="card card-body flashcard my-3">
-            <h4 class="text-capitalize">${title}</h4>
-            <a href="#" class="text-capitalize my-3 show-answer"
-              >show/hide answer</a
-            >
-            <h5 class="answer mb-3">${answer}</h5>
-            <div class="flashcard-btn d-flex justify-content-between">
-              <a
-                href="#"
-                id="edit-flashcard"
-                class="btn my-1 edit-flashcard text-uppercase"
-                data-id="${id}"
-                >edit</a
-              >
-              <a
-                href="#"
-                id="delete-flashcard"
-                class="btn my-1 delete-flashcard text-uppercase"
-                data-id="${id}"
-                >delete</a
-              >
-            </div>
-          </div>
-        </div>
-  `;
-  element.appendChild(div);
+// show feedback
+UI.prototype.feedback = function (text, element) {
+  element.classList.add('showItem', 'alert-danger');
+  element.textContent = text;
+  setTimeout(() => {
+    element.classList.remove('showItem', 'alert-danger');
+  }, 1300);
 };
-
-// clear fiedls
+// clearFiedls
 UI.prototype.clearFields = function (question, answer) {
   question.value = '';
   answer.value = '';
 };
+// add Question
+UI.prototype.addQuestion = function (data, container) {
+  const { id, question, answer } = data;
+  const div = document.createElement('div');
+  div.classList.add('col-md-4');
+  div.innerHTML = `
+  <div class="card card-body flashcard my-3">
+  <h4 class="text-capitalize">${question}</h4>
+  <a href="#" class="text-capitalize my-3 show-answer"
+    >show/hide answer</a
+  >
+  <h5 class="answer mb-3">${answer}</h5>
+  <div class="flashcard-btn d-flex justify-content-between">
+    <a
+      href="#"
+      id="edit-flashcard"
+      class="btn my-1 edit-flashcard text-uppercase"
+      data-id="${id}"
+      >edit</a
+    >
+    <a
+      href="#"
+      id="delete-flashcard"
+      class="btn my-1 delete-flashcard text-uppercase"
+      data-id="${id}"
+      >delete</a
+    >
+  </div>
+</div>
+</div>
+  `;
+  container.appendChild(div);
+};
 
-// question constructor
-function Question(id, title, answer) {
-  this.id = id;
-  this.title = title;
-  this.answer = answer;
-}
+UI.prototype.showNhide = function () {
+  document.querySelector('.show-answer').classList.toggle('showItem');
+};
 
-document.addEventListener('DOMContentLoaded', function () {
+UI.prototype.deleteQuestion = function (container, target, data) {
+  const id = target.dataset.id;
+  const parent = target.parentElement.parentElement.parentElement;
+  container.removeChild(parent);
+  data = data.filter((items) => items.id !== +id);
+  localStorage.setItem('list', JSON.stringify(data));
+};
+
+document.addEventListener('DOMContentLoaded', () => {
   eventListeners();
 });
