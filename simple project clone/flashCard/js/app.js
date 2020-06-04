3bf79ab5dbc9156c29cf9e3ab51eacb30a82023a
Leenamkwon <namkwon12@gmail.com>
2020-06-04 18:39:03 +0900

    리팩토링

--- simple project clone/flashCard/js/app.js
+++ simple project clone/flashCard/js/app.js
@@ -30,7 +30,7 @@ function eventListeners() {
       const questionData = {
         id: id++,
         question: questionValue,
-        answerInput: answerValue
+        answer: answerValue
       };
       data.push(questionData);
       ui.addQuestion(questionData, questionList);
@@ -38,16 +38,19 @@ function eventListeners() {
       localStorage.setItem('list', JSON.stringify(data));
     }
   });
+
   questionList.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.classList.contains('delete-flashcard')) {
+      // delete
       ui.deleteQuestion(questionList, e.target, data);
     } else if (e.target.classList.contains('edit-flashcard')) {
+      // edit
+      ui.editQuestion(questionList, e.target, data);
+    } else if (e.target.classList.contains('show-answer')) {
+      // 정답 공개
+      e.target.nextElementSibling.classList.toggle('answer');
     }
-  });
-  const local = JSON.parse(localStorage.getItem('list'));
-  local.forEach((item) => {
-    ui.addQuestion(item, questionList);
   });
 }
 
@@ -123,6 +126,38 @@ UI.prototype.deleteQuestion = function (container, target, data) {
   localStorage.setItem('list', JSON.stringify(data));
 };
 
+UI.prototype.editQuestion = function (container, target, data) {
+  const questionCard = document.querySelector('.question-card');
+  const feedback = document.querySelector('.feedback');
+  const questionInput = document.getElementById('question-input');
+  const answerInput = document.getElementById('answer-input');
+  const question = container.querySelector('h4');
+  const answer = container.querySelector('h5');
+  const parent = target.parentElement.parentElement.parentElement;
+  const id = target.dataset.id;
+
+  this.showQuestion(questionCard);
+  questionInput.value = question.textContent;
+  answerInput.value = answer.textContent;
+
+  container.removeChild(parent);
+  this.feedback('수정하시면 됩니다', feedback);
+  data = data.filter((items) => {
+    if (data.id === id) {
+      items.question = questionInput.value;
+      items.answer = answerInput.value;
+    }
+    return items;
+  });
+  localStorage.setItem('list', JSON.stringify(data));
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   eventListeners();
+  const ui = new UI();
+  const local = JSON.parse(localStorage.getItem('list'));
+  local.forEach((items) => {
+    const questionList = document.getElementById('questions-list');
+    ui.addQuestion(items, questionList);
+  });
 });
