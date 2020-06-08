02737c575cd7d2db311d326f170e3d066659b45b
Leenamkwon <namkwon12@gmail.com>
2020-06-08 23:32:59 +0900

    refactor

--- simple project clone/weatherApp/js/app.js
+++ simple project clone/weatherApp/js/app.js
@@ -1,12 +1,9 @@
-const form = document.getElementById('wheatherForm');
-const cityInput = document.getElementById('cityInput');
-const feedback = document.querySelector('.feedback');
-
 // class
 class AjaxWheather {
   constructor() {
     this.apikey = `9272334326503e63e6cc26603bf630e2`;
   }
+
   async getWheather(city) {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apikey}`;
     const wheatherData = await fetch(url);
@@ -15,52 +12,30 @@ class AjaxWheather {
   }
 }
 
-class Display {
-  constructor() {
-    this.result = document.querySelector('.results');
-    this.cityName = document.getElementById('cityName');
-    this.cityCountry = document.getElementById('cityCountry');
-    this.cityIcon = document.getElementById('cityIcon');
-    this.cityTemp = document.getElementById('cityTemp');
-    this.cityHumidity = document.getElementById('cityHumidity');
+(function () {
+  const form = document.getElementById('wheatherForm');
+  const cityInput = document.getElementById('cityInput');
+  const feedback = document.querySelector('.feedback');
+
+  const city = cityInput.value;
+  const ajax = new AjaxWheather();
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (city.length === 0) {
+      showFeedback('city value cannot be empty');
+    } else {
+      ajax.getWheather(city).then((data) => console.log(data));
+    }
+  });
+
+  function showFeedback(text) {
+    feedback.classList.add('showItem');
+    feedback.innerHTML = `<p>${text}</p>`;
+
+    setTimeout(() => {
+      feedback.classList.remove('showItem');
+    }, 2000);
   }
-  showWheather(data) {
-    const {
-      name,
-      sys: { country },
-      main: { temp, humidity }
-    } = data;
-
-    const { icon } = data.weather[0];
-
-    this.results.classList.add('showItem');
-    this.cityName.textContent = name;
-    this.cityCountry.textContent = country;
-    this.cityTemp.textContent = temp;
-    this.cityHumidity.textContent = humidity;
-    this.cityIcon.src = `http://openweathermap.org/img/w/${icon}.png`;
-  }
-}
-
-const ajax = new AjaxWheather();
-const display = new Display();
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const cityValue = cityInput.value;
-
-  if (cityValue.trim() === 0) {
-    showFeedback('city value cannot be empty');
-  } else {
-    ajax.getWheather(cityValue).then((data) => display.showWheather(data));
-  }
-});
-
-function showFeedback(text) {
-  feedback.classList.add('showItem');
-  feedback.innerHTML = `<p>${text}</p>`;
-
-  setTimeout(() => {
-    feedback.classList.remove('showItem');
-  }, 1300);
-}
+})();
