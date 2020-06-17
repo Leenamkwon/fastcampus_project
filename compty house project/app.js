b530e9ce21bfa2540f81df6d20d3eeb74ec49820
Leenamkwon <namkwon12@gmail.com>
2020-06-18 07:54:42 +0900

    refactor

--- compty house project/app.js
+++ compty house project/app.js
@@ -14,7 +14,7 @@ let cart = [];
 // buttons
 let buttonsDOM = [];
 
-// getting the products
+// getting the products (data module)
 class Products {
   async getProducts() {
     try {
@@ -36,101 +36,96 @@ class Products {
   }
 }
 
-// display product
+// ui module
 class UI {
-  displayItem(data) {
-    productDOM.innerHTML = '';
+  displayProducts(products) {
     let result = '';
-    data.forEach((item) => {
+    products.forEach((product) => {
       result += `
          <article class="product">
           <div class="img-container">
             <img
-              src=${item.image}
+              src=${product.image}
               alt="product"
               class="product-img"
             />
-            <button class="bag-btn" data-id=${item.id}>
+            <button class="bag-btn" data-id=${product.id}>
               <i class="fas fa-shopping-cart"></i>
               add to bag
             </button>
           </div>
-          <h3>${item.title}</h3>
-          <h4>$${item.price}</h4>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
         </article>
       `;
     });
     productDOM.innerHTML = result;
   }
 
-  getBagbuttons() {
+  getBagButtons() {
     const buttons = [...document.querySelectorAll('.bag-btn')];
     buttonsDOM = buttons;
 
     buttons.forEach((button) => {
-      const id = button.dataset.id;
-      const inCart = cart.find((item) => item.id === id);
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
 
       if (inCart) {
-        button.innerText = 'In Cart';
         button.disabled = true;
+        button.innerText = 'In Cart';
       }
 
       button.addEventListener('click', (e) => {
-        // 장바구니 버튼을 추가됐다고 텍스트를 변경한다.
         e.target.innerText = 'In Cart';
         e.target.disabled = true;
 
-        // 해당 버튼 id와 품목의 id가 같은 지 비교를 한다.
-        let products = Storage.getProducts(id);
+        // get product from products
+        let cartItem = { ...Storage.getProducts(id), amount: 1 };
 
-        // 비교한 값을 얻었으면 카트 node로 display를 해준다. (UI)
-        let arr = { ...products, amount: 1 };
+        // display cart item
+        this.addCartItem(cartItem);
 
-        // 같은 항목 끼리 빈 배열에 저장한다. (data module)
-        cart = [...cart, arr];
+        cart = [...cart, cartItem];
 
-        this.addCartItem(arr);
-
-        // 카트 아이콘의 숫자가 변경된다.
-        this.setValueCart(cart);
-
-        // 카트돔에 데이터 ui가 들어갔는지 보여야 한다.
-        this.showCart();
-
-        // 카트를 로컬스토리지에 저장한다.
+        // local storage save cart
         Storage.saveCart(cart);
+
+        // set cart values
+        this.setCartValues(cart);
+
+        // show cart
+        this.showCart();
       });
     });
   }
 
-  setValueCart(cart) {
+  setCartValues(cart) {
     let tempTotal = 0;
-    let itemsTotal = 0;
+    let itemTotal = 0;
     cart.map((item) => {
       tempTotal += item.price * item.amount;
-      itemsTotal += item.amount;
+      itemTotal += item.amount;
     });
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
-    cartItems.innerText = itemsTotal;
+    cartItems.innerText = itemTotal;
   }
 
-  addCartItem(cart) {
+  addCartItem(item) {
     const div = document.createElement('div');
     div.classList.add('cart-item');
     div.innerHTML = `
-            <img src=${cart.image} alt="product" />
+            <img src=${item.image} alt="product" />
             <div>
-              <h4>${cart.title}</h4>
-              <h5>$${cart.price}</h5>
-              <span class="remove-item" data-id=${cart.id}>remove</span>
+              <h4>${item.title}</h4>
+              <h5>$${item.price}</h5>
+              <span class="remove-item" data-id=${item.id}>remove</span>
             </div>
             <div>
-              <i class="fas fa-chevron-up" data-id=${cart.id}></i>
-              <p class="item-amount" data-id=${cart.id}>1</p>
-              <i class="fas fa-chevron-down" data-id=${cart.id}></i>
+              <i class="fas fa-chevron-up" data-id=${item.id}></i>
+              <p class="item-amount">1</p>
+              <i class="fas fa-chevron-down" data-id=${item.id}></i>
             </div>
-      `;
+        `;
     cartContent.appendChild(div);
   }
 
@@ -141,18 +136,18 @@ class UI {
 
   hideCart() {
     cartOverlay.classList.remove('transparentBcg');
-    cartDOM.classList.remove('showCart');
+    cartDOM.classList.remove('showcart');
   }
 
   setupAPP() {
     cart = Storage.getCart();
-    this.setValueCart(cart);
-    this.populateCart(cart);
+    this.setCartValues(cart);
+    this.populate(cart);
     cartBtn.addEventListener('click', this.showCart);
     closeCartBtn.addEventListener('click', this.hideCart);
   }
 
-  populateCart(cart) {
+  populate(cart) {
     cart.forEach((item) => this.addCartItem(item));
   }
 
@@ -162,66 +157,48 @@ class UI {
     });
     cartContent.addEventListener('click', (e) => {
       if (e.target.classList.contains('remove-item')) {
-        this.removeSingleItem(e);
-      } else if (e.target.classList.contains('fa-chevron-up')) {
-        let addAmount = e.target;
-        let id = addAmount.dataset.id;
-        let tempItem = cart.find((item) => item.id === id);
-        tempItem.amount++;
+        cart = cart.filter((item) => item.id !== e.target.dataset.id);
+        this.setCartValues(cart);
         Storage.saveCart(cart);
-        this.setValueCart(cart);
-        addAmount.nextElementSibling.innerText = tempItem.amount;
-      } else if ('fa-chevron-down') {
-        let addAmount = e.target;
-        let id = addAmount.dataset.id;
-        let tempItem = cart.find((item) => item.id === id);
-        tempItem.amount++;
-        addAmount.nextElementSibling.innerText = tempItem.amount;
-        Storage.saveCart(cart);
-        this.setValueCart(cart);
+        const parent = e.target.parentElement.parentElement;
+        cartContent.removeChild(parent);
       }
     });
   }
 
   clearCart() {
     let cartItems = cart.map((item) => item.id);
-    cartItems.forEach((id) => this.removeItem(id));
-
+    cartItems.forEach((id) => {
+      this.removeItem(id);
+    });
     while (cartContent.children.length > 0) {
       cartContent.removeChild(cartContent.children[0]);
     }
   }
 
   removeItem(id) {
-    cart = cart.filter((cartID) => cartID.id !== id);
-    this.setValueCart(cart);
-    Storage.saveCart(cart);
+    cart = cart.filter((item) => item.id !== id);
     let button = this.getSingleButton(id);
     button.disabled = false;
-    button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
+    button.innerText = 'Add Cart';
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
   }
 
   getSingleButton(id) {
-    return buttonsDOM.find((button) => button.dataset.id === id);
-  }
-
-  removeSingleItem(e) {
-    cart = cart.filter((cartID) => cartID.id !== e.target.dataset.id);
-    this.setValueCart(cart);
-    const parent = e.target.parentElement.parentElement;
-    cartContent.removeChild(parent);
+    return buttonsDOM.find((item) => item.dataset.id === id);
   }
 }
 
-// local Storage
+// local storage module
 class Storage {
   static saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
   }
 
   static getProducts(id) {
-    const product = JSON.parse(localStorage.getItem('products'));
-    return product.find((item) => item.id === id);
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find((item) => item.id === id);
   }
 
   static saveCart(cart) {
@@ -239,18 +216,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
 
-  // setup APP
+  // setup app
   ui.setupAPP();
 
-  // get all products
   products
     .getProducts()
     .then((products) => {
-      ui.displayItem(products);
       Storage.saveProducts(products);
+      ui.displayProducts(products);
     })
     .then(() => {
-      ui.getBagbuttons();
+      ui.getBagButtons();
       ui.cartLogic();
     });
 });
