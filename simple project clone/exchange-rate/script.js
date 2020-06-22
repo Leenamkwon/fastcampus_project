96ca96066bc731e01b98850e9fed88f792d7bca7
Leenamkwon <namkwon12@gmail.com>
2020-06-22 12:32:06 +0900

    new year

--- simple project clone/exchange-rate/script.js
+++ simple project clone/exchange-rate/script.js
@@ -6,35 +6,34 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-// fetch exchange rates and update the DOM
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then((res) => res.json())
+    .then((data) => data.json())
     .then((data) => {
       const rate = data.rates[currency_two];
 
-      rateEl.innerText = `${data.date} 기준: 100 ${currency_one} = ${
-        rate * 100
-      } ${currency_two}`;
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
 }
 
-// event listeners
-currencyEl_one.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
-currencyEl_two.addEventListener('change', calculate);
-amountEl_two.addEventListener('input', calculate);
-
-swap.addEventListener('click', () => {
+function swap2() {
   const temp = currencyEl_one.value;
   currencyEl_one.value = currencyEl_two.value;
   currencyEl_two.value = temp;
   calculate();
-});
+}
 
-calculate();
+const setUpEventListener = function () {
+  calculate();
+  currencyEl_one.addEventListener('change', calculate);
+  amountEl_one.addEventListener('input', calculate);
+  currencyEl_two.addEventListener('change', calculate);
+  swap.addEventListener('click', swap2);
+};
+
+setUpEventListener();
