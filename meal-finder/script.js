3094dec5d302ee07ba6b766b4392045bc6822df2
Leenamkwon <namkwon12@gmail.com>
2020-06-23 09:04:02 +0900

    repeat and draggable list

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -20,16 +20,17 @@ function searchMeal(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
         if (data.meals === null) {
-          resultHeading.innerHTML = 'There are no search results. Try again!';
+          resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
         } else {
           mealsEl.innerHTML = data.meals
             .map(
               (meal) => `
             <div class="meal">
-              <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
               <div class="meal-info" data-mealID="${meal.idMeal}">
                 <h3>${meal.strMeal}</h3>
               </div>
@@ -42,13 +43,28 @@ function searchMeal(e) {
     // Clear search text
     search.value = '';
   } else {
-    alert('Please enter a search value');
+    alert('Please enter a search term');
   }
 }
 
 // Fetch meal by ID
-function getMealById(mealId) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+// Fetch random meal from API
+function getRandomMeal() {
+  // Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
     .then((res) => res.json())
     .then((data) => {
       const meal = data.meals[0];
@@ -59,11 +75,11 @@ function getMealById(mealId) {
 
 // Add meal to DOM
 function addMealToDOM(meal) {
-  let ingredient = [];
-  //
-  for (let i = 1; i <= 20; i += 1) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
     if (meal[`strIngredient${i}`]) {
-      ingredient.push(
+      ingredients.push(
         `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
       );
     } else {
@@ -83,35 +99,18 @@ function addMealToDOM(meal) {
         <p>${meal.strInstructions}</p>
         <h2>Ingredients</h2>
         <ul>
-          ${ingredient.map((item) => `<li>${item}</li>`).join('')}
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
         </ul>
       </div>
     </div>
   `;
 }
 
-// Fetch random meal from API
-function getRandomMeal() {
-  // clear meals and heading
-  mealsEl.innerHTML = '';
-  resultHeading.innerHTML = '';
-
-  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-
-      const meal = data.meals[0];
-
-      addMealToDOM(meal);
-    });
-}
-
+// Event listeners
 submit.addEventListener('submit', searchMeal);
 random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', (e) => {
-  e.preventDefault();
   const mealInfo = e.path.find((item) => {
     if (item.classList) {
       return item.classList.contains('meal-info');
---
+++ simple project clone/draggable-list/script.js
@@ -0,0 +1,109 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page'
+];
+
+const listItems = [];
+
+let dragStartIndex;
+
+createListItem();
+
+function createListItem() {
+  [...richestPeople]
+    .map((item) => ({ value: item, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((item) => item.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
+      listItem.setAttribute('data-index', index);
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+      <p class="person-name">${person}</p>
+      <i class="fas fa-grip-lines"></i>
+    </div>
+    `;
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
+
+  addEventListener();
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+  this.classList.add('over');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {
+  this.classList.remove('over');
+}
+
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove('over');
+}
+
+function swapItems(FromIndex, toIndex) {
+  const itemOne = listItems[FromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[FromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+
+  listItems.forEach((item) => {
+    console.log(item);
+  });
+}
+
+function addEventListener() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
+}
+
+function checkOrder() {
+  listItems.forEach((item, index) => {
+    const order = listItems[index]
+      .querySelector('.person-name')
+      .innerText.trim();
+    if (richestPeople[index] === order) {
+      listItems[index].classList.remove('wrong');
+      listItems[index].classList.add('right');
+    } else {
+      listItems[index].classList.add('wrong');
+    }
+  });
+}
+
+check.addEventListener('click', checkOrder);
