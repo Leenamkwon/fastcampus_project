1663f22aff3bfce8618d4577421681336e19827e
Leenamkwon <namkwon12@gmail.com>
2020-06-24 16:16:11 +0900

    Netflix landing page complete

--- Netflix Landing Page/app.js
+++ Netflix Landing Page/app.js
@@ -1,30 +1,38 @@
-const tabs = document.querySelectorAll('.tab-item');
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 const border = document.querySelector('.tab-border');
-
-let index = 0;
 
 function borderMoving() {
   const width = event.target.parentElement.getBoundingClientRect();
   const parent = event.target.parentElement;
-  border.style.width = `${width}px`;
   border.classList.add('trans');
 
-  if (parent.id === 'tab-2') {
+  if (parent.id === 'tab-1') {
+    border.style.transform = `translateX(${0}px)`;
+  } else if (parent.id === 'tab-2') {
     border.style.transform = `translateX(${width.width + 16}px)`;
   } else if (parent.id === 'tab-3') {
     border.style.transform = `translateX(${width.width * 2 + 32}px)`;
-  } else if (parent.id === 'tab-1') {
-    border.style.transform = `translateX(${0}px)`;
   }
 }
 
-tabs.forEach((tab) => {
-  tab.addEventListener('click', () => {
-    borderMoving();
-  });
-});
+function selecItem(self) {
+  removeShow();
+  // Grab content item dom
+  console.log(self.id);
 
-document.addEventListener('resize', () => {
-  const width = event.target.parentElement.getBoundingClientRect();
-  border.style.width = `${width}px`;
+  const tabContentItem = document.querySelector(`#${self.id}-content`);
+  tabContentItem.classList.add('show');
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove('show'));
+}
+
+tabItems.forEach((tab) => {
+  tab.addEventListener('click', function () {
+    const self = this;
+    borderMoving();
+    selecItem(self);
+  });
 });
