3d3941a18469c6bdb6ac306bb6ef772716d034bb
Leenamkwon <namkwon12@gmail.com>
2020-07-06 07:47:48 +0900

    search controller add

--- Cocktailer/app.js
+++ Cocktailer/app.js
@@ -1,9 +1,12 @@
 import showDrink from './src/presentDrinks.js';
+import search from './src/searchForm.js';
 
-const proxy = `https://cors-anywhere.herokuapp.com/`;
 let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=d`;
 const detailID = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`;
 
 window.addEventListener('DOMContentLoaded', () => {
   showDrink(url);
 });
+
+// search form controller
+search();
--- Cocktailer/src/searchForm.js
+++ Cocktailer/src/searchForm.js
@@ -1,1 +1,22 @@
-import get from './getElement';
+import get from './getElement.js';
+import presentDrinks from './presentDrinks.js';
+
+const baseURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
+
+const form = get('.search-form');
+const input = get('[name="drink"]');
+
+console.log(form);
+
+const search = () => {
+  form.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    if (!input.value) {
+      return;
+    } else if (input.value.trim()) {
+      presentDrinks(`${baseURL}${input.value}`);
+    }
+  });
+};
+
+export default search;
