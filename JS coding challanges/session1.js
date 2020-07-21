cb0fadbb6e4372a3c198876702ce6e141de347e3
Leenamkwon <namkwon12@gmail.com>
2020-07-21 11:43:15 +0900

    fizz buzz

--- JS coding challanges/session1.js
+++ JS coding challanges/session1.js
@@ -97,34 +97,44 @@ function capitalizeLetters(str) {
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str) {
 	/* My solution 1 */
-	const charMap = {};
-	let maxNum = 0;
-	let maxChar = '';
-
-	str.split('').forEach((char) => {
-		if (charMap[char]) {
-			charMap[char]++;
-		} else {
-			charMap[char] = 1;
-		}
-	});
-
-	console.log(charMap);
-
-	for (let item in charMap) {
-		if (charMap[item] > maxNum) {
-			maxNum = charMap[item];
-			maxChar = item;
-		}
-	}
-	return maxChar;
+	// const charMap = {};
+	// let maxNum = 0;
+	// let maxChar = '';
+	// str.split('').forEach((char) => {
+	// 	if (charMap[char]) {
+	// 		charMap[char]++;
+	// 	} else {
+	// 		charMap[char] = 1;
+	// 	}
+	// });
+	// for (let item in charMap) {
+	// 	if (charMap[item] > maxNum) {
+	// 		maxNum = charMap[item];
+	// 		maxChar = item;
+	// 	}
+	// }
+	// return maxChar;
 }
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+	/* My solution */
+	for (let i = 1; i <= 100; i++) {
+		if (i % 15 === 0) {
+			console.log('FizzBuzz');
+		} else if (i % 3 === 0) {
+			console.log('Fizz');
+		} else if (i % 5 === 0) {
+			console.log('Buzz');
+		} else {
+			console.log(i);
+			continue;
+		}
+	}
+}
 
 // Call Function
-const output = maxCharacter('jjjjavascript');
+const output = fizzBuzz();
 
 console.log(output);
