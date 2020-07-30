5b9d0b0aca216cae9c6c9fbee46f8354f70306b0
Leenamkwon <namkwon12@gmail.com>
2020-07-31 07:07:06 +0900

    debounce

--- design pattern/movie/index.js
+++ design pattern/movie/index.js
@@ -1,1 +1,31 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '11ac11dc',
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('.input');
+
+const debounce = (func) => {
+  let timeoutId;
+  return (asd) => {
+    console.log(asd);
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func(asd);
+    }, 1000);
+  };
+};
+
+const onInput = debounce((event) => {
+  fetchData(event.target.value);
+});
+
+input.addEventListener('input', onInput);
