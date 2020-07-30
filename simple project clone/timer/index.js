e214abd267736ef908516bc323ee49b3a9112415
Leenamkwon <namkwon12@gmail.com>
2020-07-31 01:43:16 +0900

    svg elements

--- simple project clone/timer/index.js
+++ simple project clone/timer/index.js
@@ -1,38 +1,19 @@
-class Timer {
-  constructor(startBtn, pauseBtn, durationInput) {
-    this.durationInput = durationInput;
-    this.startBtn = startBtn;
-    this.pauseBtn = pauseBtn;
-
-    this.timeLeft = 0;
-
-    this.startBtn.addEventListener('click', this.start);
-    this.pauseBtn.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    this.timeLeft = durationInput.value;
-    this.timeLeft--;
-    this.durationInput.value = this.timeLeft;
-
-    if (this.timeLeft <= 0) {
-      this.pause();
-      this.timeLeft = 0;
-    }
-  };
-}
+import Timer from './timer.js';
 
 const durationInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 
-const timer = new Timer(startBtn, pauseBtn, durationInput);
+const timer = new Timer(startBtn, pauseBtn, durationInput, {
+  onStart() {
+    console.log('Timer started');
+  },
+  onTick() {
+    console.log('Timer just ticked down');
+  },
+  onComplete() {
+    console.log('Timer is completed');
+  },
+});
+
+timer;
