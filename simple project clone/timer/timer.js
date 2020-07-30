5fda2c398cd191258f36603ca2a3f3ba0b9406d7
Leenamkwon <namkwon12@gmail.com>
2020-07-31 02:30:47 +0900

    design pattern

--- simple project clone/timer/timer.js
+++ simple project clone/timer/timer.js
@@ -1,26 +1,24 @@
-export default class Timer {
-  constructor(startBtn, pauseBtn, durationInput, callbacks) {
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
-    this.startBtn = startBtn;
-    this.pauseBtn = pauseBtn;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
     if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
       this.onComplete = callbacks.onComplete;
     }
 
-    this.timeLeft = 0;
-
-    this.startBtn.addEventListener('click', this.start);
-    this.pauseBtn.addEventListener('click', this.pause);
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
     if (this.onStart) {
-      this.onStart();
+      this.onStart(this.timeRemaining);
     }
     this.tick();
-    this.interval = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 20);
   };
 
   pause = () => {
@@ -30,10 +28,15 @@ export default class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
-      if (this.onComplete) this.onComplete();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.02;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
     }
-    this.timeRemaining = this.timeRemaining - 1;
-    if (this.onTick) this.onTick();
   };
 
   get timeRemaining() {
@@ -41,6 +44,6 @@ export default class Timer {
   }
 
   set timeRemaining(time) {
-    this.durationInput.value = time;
+    this.durationInput.value = time.toFixed(2);
   }
 }
