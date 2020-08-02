a52da4501c05e5e0ec8e79cda6ed96fb87552c6b
Leenamkwon <namkwon12@gmail.com>
2020-08-02 10:10:26 +0900

    timer start

--- simple project clone/timer/index.js
+++ simple project clone/timer/index.js
@@ -1,23 +1,31 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    console.log(this);
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start = () => {
+    this.tick();
+    this.timer = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.timer);
+  };
+
+  tick = () => {
+    console.log('tick');
+  };
+}
+
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
-const circle = document.querySelector('circle');
 
-const perimeter = circle.getAttribute('r') * 2 * Math.PI;
-circle.setAttribute('stroke-dasharray', perimeter);
+const timer = new Timer(durationInput, startButton, pauseButton);
 
-let duration;
-const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart(totalDuration) {
-    duration = totalDuration;
-  },
-  onTick(timeRemaining) {
-    circle.setAttribute(
-      'stroke-dashoffset',
-      (perimeter * timeRemaining) / duration - perimeter
-    );
-  },
-  onComplete() {
-    console.log('Timer is completed');
-  }
-});
+timer.start();
