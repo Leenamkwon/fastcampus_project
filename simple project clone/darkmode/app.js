19bb2fa6aae64942ff169e7108b003b72c659f17
Leenamkwon <namkwon12@gmail.com>
2020-08-20 05:16:18 +0900

    repo fix

--- simple project clone/darkmode/app.js
+++ simple project clone/darkmode/app.js
@@ -2,30 +2,30 @@ const DARKBTN = document.querySelector('.dark-mode-toggle');
 const body = document.body;
 
 document.addEventListener('DOMContentLoaded', () => {
-	getMode();
+  getMode();
 });
 
 DARKBTN.addEventListener('click', (e) => {
-	if (e.target.closest('#dark-mode-toggle')) {
-		isDark();
-	}
+  if (e.target.closest('#dark-mode-toggle')) {
+    isDark();
+  }
 });
 
 function isDark() {
-	if (!body.classList.contains('darkmode')) {
-		body.classList.add('darkmode');
-		setMode('darkmode');
-	} else {
-		body.className = '';
-		localStorage.removeItem('Mode');
-	}
+  if (!body.classList.contains('darkmode')) {
+    body.classList.add('darkmode');
+    setMode('darkmode');
+  } else {
+    body.classList.remove('darkmode');
+    localStorage.removeItem('Mode');
+  }
 }
 
 function getMode() {
-	const isGet = localStorage.getItem('Mode');
-	isGet !== null ? body.classList.add(isGet) : '';
+  const isGet = localStorage.getItem('Mode');
+  isGet !== null ? body.classList.add(isGet) : '';
 }
 
 function setMode(val) {
-	localStorage.setItem('Mode', val);
+  localStorage.setItem('Mode', val);
 }
